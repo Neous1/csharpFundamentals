c54f81fb86a842384fd293152bc66ea24dd4e929
Neous1 <Neous1@users.noreply.github.com>
2017-10-16 09:37:53 -0700

    minmax

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+
         }
     }
 }
