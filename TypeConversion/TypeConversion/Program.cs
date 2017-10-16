18d41f37d854273bae3d05b071867dc37858bd4a
Neous1 <Neous1@users.noreply.github.com>
2017-10-16 09:54:01 -0700

    byte

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1; //1 byte
+            int i = b; // 4 bytes
+            Console.WriteLine(i);
+        }
+    }
+}
