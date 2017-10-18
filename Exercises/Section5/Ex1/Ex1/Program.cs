db885fa4350edce9b3096d74bc131ef30ac152ac
Neous1 <Neous1@users.noreply.github.com>
2017-10-18 08:49:50 -0700

    Exercise 1 : return Valid for integer

---
+++ Exercises/Section5/Ex1/Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter a number between 1-10");
+
+            var userInput = Int32.Parse(Console.ReadLine());
+            if (userInput >= 0 && userInput <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+        }
+ 
+    }
+}
