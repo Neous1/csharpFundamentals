7ed9387cac31535137bcc203f2b18a484a1c8860
Neous1 <Neous1@users.noreply.github.com>
2017-10-18 09:57:36 -0700

    Ex 3 complete

---
+++ Exercises/Section5/Ex3/Ex3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the width of the frame");
+            var width = int.Parse(Console.ReadLine());
+            Console.WriteLine("What's the height of the frame");
+
+            var height = int.Parse(Console.ReadLine());
+
+            if (width > height)
+            {
+                Console.WriteLine("the image is Landscape");
+            }
+            else
+            {
+                Console.WriteLine("the image is Portrait");
+            }
+        }
+    }
+}
