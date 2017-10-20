2894dd7b1e91530c22fce19242b572278f0fed82
Neous1 <Neous1@users.noreply.github.com>
2017-10-20 12:35:21 -0700

    Ex2 initial

---
+++ Exercises/Section5/Lecture48/Ex.cs/Ex.cs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex.cs
+{
+    class Program
+    {
+
+        //static int calculate()
+        //{
+        //    sum += entry;
+        //}
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            
+            Console.WriteLine("enter a number or Ok ");
+              entry. = Console.ReadLine();
+            if (entry != "ok" || entry != "OK")
+            {
+                entry = int.Parse(Console.ReadLine());
+                //sum += entry;
+                return;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
