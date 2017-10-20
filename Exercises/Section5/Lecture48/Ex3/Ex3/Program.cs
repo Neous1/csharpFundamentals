8d5307661690e8223b5dc1bc60b85d89644b2ebd
Neous1 <Neous1@users.noreply.github.com>
2017-10-19 19:48:54 -0700

    complete Ex3 of lecture 48

---
+++ Exercises/Section5/Lecture48/Ex3/Ex3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            int result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int x = Factorial(n);
+            Console.WriteLine(n+"!= "+x);
+        }
+    }
+}
---
+++ Exercises/Section5/Lecture48/Exercises/Exercises/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+            //    if (args == null) throw new ArgumentNullException(nameof(args));
+            //    int count = 0;
+            //    for (var i = 0; i <= 100; i++)
+            //    {
+            //        if (i % 3 == 0)
+            //        {
+            //            Console.WriteLine(i);
+            //            count++;
+            //        }
+            //    }
+            //    Console.WriteLine("count is :"+count);
+            Ex3();
+        }
+
+        //public void Ex2()
+        //{
+        //    while (Console.ReadLine() != "ok")
+        //    {
+        //        Console.WriteLine();
+        //    }
+        //}
+        /*
+        write a program and ask the user to enter a number. 
+        Computer the factorial of the nbmer and print it onthe console. 
+        For example, if the user enters 5, the program shold calculate 
+        5x4x3x2x1 and display it as 5! = 120
+        */
+        public static int Ex3()
+        {
+            //int result = 0;
+            //Console.WriteLine("Enter a number");
+            //Console.ReadLine();
+            ////var n = int.Parse(Console.ReadLine());
+            //var n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(n);
+
+            //while (n >= 1)
+            //{
+            //     result = n * (n - 1);
+            //    n--;
+            //}
+            //Console.WriteLine(result);
+
+            //*****************************************************
+            Console.WriteLine("Enter a number");
+            var n = Convert.ToInt32(Console.ReadLine());
+            if (n <= 1)
+            {
+                return 1;
+                
+            }
+            int result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result = result * i;
+            }
+
+            return result;
+            Console.WriteLine(result);
+        }
+        
+        
+
+
+
+    }
+}
