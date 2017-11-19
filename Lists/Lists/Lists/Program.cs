90804e6cf610887d64282662cc79c4ba276668da
Neous1 <Neous1@users.noreply.github.com>
2017-11-18 22:50:51 -0800

    declare a list and initialize it.

---
+++ Lists/Lists/Lists/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() {1,2,3,4};
+        }
+    }
+}
+ 
