d24fa2d83d5e2ab385d39eb92ee914d3e6d9c918
Neous1 <Neous1@users.noreply.github.com>
2017-11-18 19:09:19 -0800

    Ex4 generate random num

---
+++ Exercises/Section5/Lecture48/Ex4/Ex4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            ///pick a number between 1 and 10
+            /// prompt user to select a number 
+            /// if user number == computer number ; 
+            /// display sign you won
+            /// else you loose.
+            /// 
+            
+            Random random = new Random();
+            int comp = random.Next(0, 10);
+
+            Console.WriteLine("guess which number");
+            Console.ReadLine("")
+            Console.WriteLine(comp);
+
+
+        }
+    }
+}
