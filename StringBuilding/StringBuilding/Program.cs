00cd564d1fd6b6e86d29ea8bfe703f16e8218707
Neous1 <Neous1@users.noreply.github.com>
2017-11-20 17:31:40 -0500

    add new line or characters on existing strings

--- StringBuilding/StringBuilding/Program.cs
+++ StringBuilding/StringBuilding/Program.cs
@@ -10,10 +10,17 @@ namespace StringBuilding
     {
         static void Main(string[] args)
         {
-                var builder = new StringBuilder("Hello World");
-                builder.Append('-', 10);
+            var builder = new StringBuilder("Hello World");
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
 
-                Console.WriteLine(builder);
+
+
+
+            Console.WriteLine(builder);
             
         }
     }
