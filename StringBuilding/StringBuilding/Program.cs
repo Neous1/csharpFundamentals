49d77b6cd7d932609f4d80c17673b058258620c9
Neous1 <Neous1@users.noreply.github.com>
2017-11-20 17:47:16 -0500

    the methods return stringbuilder as a type so they can be daisy chained

--- StringBuilding/StringBuilding/Program.cs
+++ StringBuilding/StringBuilding/Program.cs
@@ -11,18 +11,18 @@ namespace StringBuilding
         static void Main(string[] args)
         {
             var builder = new StringBuilder("Hello World");
-            builder.Append('-', 10);
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.AppendLine();
-            builder.Append('-', 10);
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10) // remove 10 character starting at index 0
+                .Insert(0, new string('-', 10));
 
-            builder.Replace('-', '+');
-
-            builder.Remove(0, 10); // remove 10 character starting at index 0
-
-            builder.Insert(0, new string('-', 10));
             Console.WriteLine(builder);
+            Console.WriteLine("First Char: " + builder[0]);
             
         }
     }
