e99da8838277c0e970bb77c2ad468ff8a22c340d
Neous1 <Neous1@users.noreply.github.com>
2017-11-20 14:09:54 -0500

    add Ex2 to section 6 exercises

--- Exercises/Section6/Ex2/Ex2/Program.cs
+++ Exercises/Section6/Ex2/Ex2/Program.cs
@@ -4,23 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ex1
+namespace Ex2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ///create a list
-            /// add a name
-            /// if count = 1 display name added
-            /// if count = 2 diplay name [0] and name [1]
-            /// if count > 2 diplay name [0] and name [1] and count
-
-
-
-            //prompt user for name
-            //display reversed name using array.reverse();
-
             Console.WriteLine("Please enter you name");
             var name = Console.ReadLine();
             if (name != null)
@@ -28,10 +17,6 @@ namespace Ex1
                 name = new string(name.Reverse().ToArray());
             }
             Console.WriteLine(name);
-
-
-
-
         }
     }
 }
