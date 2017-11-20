df83c7dc011fe1265b05c01871417140af5e613b
Neous1 <Neous1@users.noreply.github.com>
2017-11-20 14:52:02 -0500

    Replace()

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -24,6 +24,7 @@ namespace Strings
             Console.WriteLine("FirstName: "+ names[0]);
             Console.WriteLine("LastName: "+ names[1]);
 
+            Console.WriteLine(fullName.Replace("Joel", "Jolie"));
         }
     }
 }
