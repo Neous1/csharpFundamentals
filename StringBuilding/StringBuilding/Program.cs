d07554ca1166289dcbd7e226cdd7ddffd0dcc368
Neous1 <Neous1@users.noreply.github.com>
2017-11-20 17:28:33 -0500

    Append()

---
+++ StringBuilding/StringBuilding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                var builder = new StringBuilder("Hello World");
+                builder.Append('-', 10);
+
+                Console.WriteLine(builder);
+            
+        }
+    }
+}
