1ebbeb3a99d02389376c051d814d3795a13e92ed
Neous1 <Neous1@users.noreply.github.com>
2017-11-20 14:34:34 -0500

    Add TimeSpan

--- DateTimeNew/DateTimeNew/Program.cs
+++ DateTimeNew/DateTimeNew/Program.cs
@@ -15,6 +15,17 @@ namespace DateTimeNew
             var today = DateTime.Today;
 
             Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
+            var tomorow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+
         }
     }
 }
---
+++ TimeSpan/TimeSpan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TimeSpan;
+
+namespace TimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeSpan = new System.TimeSpan(1, 2, 3);
+
+            var timeSpan1 = new System.TimeSpan(1, 0, 0);
+            var timeSpan2 = System.TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration :" + duration);
+
+
+        }
+    }
+}
