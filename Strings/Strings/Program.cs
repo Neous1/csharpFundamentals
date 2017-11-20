862dd0eaab51109b1ee6a158376f05eb59a2f264
Neous1 <Neous1@users.noreply.github.com>
2017-11-20 14:51:26 -0500

    substring determine by index

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -14,6 +14,12 @@ namespace Strings
             Console.WriteLine("trim: '{0}'", fullName.Trim());
             Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
 
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index +1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
 
         }
     }
