e52d076bd2a3b4efe08889a6c094e42b3d6f0d54
Neous1 <Neous1@users.noreply.github.com>
2017-11-20 19:02:14 -0500

    display files from directory

---
+++ Directories/Directories/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\users\yvon\dev\csharpFundamentals", "*.sln", SearchOption.AllDirectories);
+            foreach(var file in files)
+                Console.WriteLine(file);
+        }
+    }
+}
